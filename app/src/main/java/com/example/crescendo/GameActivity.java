eec0746a276424e16a8698a5abdd2f8917e1ab33
JC <jaychaplotfl@gmail.com>
2024-04-11 02:40:51 -0400

    Got the guessing artist to work, with cancel and other toast options

--- app/src/main/java/com/example/crescendo/GameActivity.java
+++ app/src/main/java/com/example/crescendo/GameActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -29,8 +32,6 @@ import okhttp3.Request;
 
 public class GameActivity extends AppCompatActivity {
 
-    private TextView displayArtistNameTextView;
-    private Button showInputDialogButton
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +39,46 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         ImageView avatarImageView = findViewById(R.id.avatarImageView);
-        displayArtistNameTextView = findViewById(R.id.displayArtistNameTextView);
-        showInputDialogButton = findViewById(R.id.showInputDialogButton);
+        TextView displayArtistNameTextView = findViewById(R.id.displayArtistNameTextView);
+        Button showInputDialogButton = findViewById(R.id.showInputDialogButton);
 
         Glide.with(getApplicationContext()).load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkliO9xHJ0NrIYKvWrTl5Hfsfs0JRkSnsPFEQIKnNvNA&s").placeholder(R.drawable.baseline_image_24).into(avatarImageView);
         ArrayList<String> myList = new ArrayList<>();
         myList.add("Bruno Mars");
 
+        displayArtistNameTextView.setText("Bruno Mars");
+        showInputDialogButton.setOnClickListener(view -> {
+            showInputDialog();
+        });
+    }
 
+        private void showInputDialog() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Enter Artist Name");
+
+            final EditText input = new EditText(this);
+            builder.setView(input);
+
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String userInput = input.getText().toString().toLowerCase();
+                    if ("Bruno Mars".toLowerCase().equals(userInput)) {
+                        Toast.makeText(GameActivity.this, "YOU GUESSED CORRECT!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(GameActivity.this, "WRONG!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            builder.show();
+        }
        /* displayArtistNameTextView = findViewById(R.id.displayArtistNameTextView);
 
         ArrayList<String> myList = new ArrayList<>();
@@ -83,5 +116,5 @@ public class GameActivity extends AppCompatActivity {
     }
 
 
-}
+
 
