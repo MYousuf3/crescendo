b761e0e29bf89eb093396cfa9c1f3c065594ae21
tanmayhaldiya <tanmayhaldiya@gmail.com>
2024-04-12 03:38:03 -0400

    added quips

--- app/src/main/java/com/example/crescendo/TransitionActivity.java
+++ app/src/main/java/com/example/crescendo/TransitionActivity.java
@@ -5,10 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class TransitionActivity extends AppCompatActivity {
 
     public static final String NEXT_ACTIVITY_KEY = "next_activity";
+    Random rand;
+    TextView transitionText;
+    String[] quips = new String[]{"But wait, there's more.",
+                                  "Drumroll please...",
+                                  "You're not ready for this",
+                                  "Hold on to your seats...",
+                                  "And now for the big reveal...",
+                                  "That's not all...",
+                                  "Cue the fanfare...",
+                                  "Hold your applause...",
+                                  "Get ready for this...",
+                                  "It's called crescendo because it only gets better from here..."};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,15 @@ public class TransitionActivity extends AppCompatActivity {
 
         // Determine next activity based on the extra passed with the intent
         String nextActivity = getIntent().getStringExtra(NEXT_ACTIVITY_KEY);
+
+        rand = new Random();
+        transitionText = findViewById(R.id.transitionText);
+        int index = rand.nextInt(quips.length);
+        String quip = quips[index];
+
+
+        transitionText.setText(quip);
+
         new Handler().postDelayed(() -> {
             Intent intent;
             switch (nextActivity) {
