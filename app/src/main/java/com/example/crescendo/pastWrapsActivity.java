472b6027d2082c95cfe02162e80b8ac63dff602b
Shadi Buchanan <shadibuchanan04@gmail.com>
2024-04-19 12:43:20 -0400

    Created past wraps screen and initialized activity

--- app/src/main/java/com/example/crescendo/pastWrapsActivity.java
+++ app/src/main/java/com/example/crescendo/pastWrapsActivity.java
@@ -10,5 +10,6 @@ public class pastWrapsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_wraps);
+
     }
 }
